3c13ee7e3fc0b729b4c920b97fda9bccd5be2549
hexadivine <hexadivine@gmail.com>
2024-11-07 23:47:49 +1100

    feat: improve code

--- ezrecon.functions.sh
+++ ezrecon.functions.sh
@@ -32,9 +32,31 @@ function getOpenPortList() {
 
 function nmapRemainingFullPortScan() {
     local knownPorts=$1
-    nmapScan "6-full-port-scan" "-v -p-"
-    openPorts=$(getOpenPortList "1-common-ports-scan")
-    echo $openPorts
+    nmapScan "6-full-port-scan" "-v -p- --host-timeout 60m"
+    local openPorts=$(getOpenPortList "6-full-port-scan")
+
+    for openPort in $(echo $openPorts | tr ',' '\n'); do
+        if [[ ! ",$knownPorts," =~ ",$openPort,"  ]]; then  
+            echo $openPort >> './1-nmap/7-new-ports.txt'
+        fi
+    done
+
+    if [[ ! -f "./1-nmap/7-new-ports.txt" ]]; then
+        echo '[x] New ports are not found'
+        return -1
+    fi
+
+    echo '[+] Found remaining ports...'
+
+    local newPorts=$(cat "./1-nmap/7-new-ports.txt" | tr '\n' ',')
+    newPorts="${newPorts%,}"
+
+    nmapScan "8-new-remaining-general-scan" "-sCSV -p$newPorts" &
+    nmapScan "9-new-remaining-vul-scan" "--script vuln -p$newPorts" &
+    nmapScan "10-new-remaining-aggressive-scan" "-A -p$newPorts" &
+    nmapScan "11-new-remaining-version-scan" "-sV -p$newPorts" &
+
+
 }
 
 function ffufScan() {
